8a098929aa7dabe52a749a172d576d4f4eafd482
IE729892 <ie729892@iteso.mx>
2024-05-06 19:03:00 -0600

    Test for pipes and NN w/pipes

--- PIPES_EN_PYTHON/Python_pipes.py
+++ PIPES_EN_PYTHON/Python_pipes.py
@@ -1,37 +1,37 @@
 import os
+#pull en master y nueva branch ahi
 
 # Crear pipe con nombre
-pipe_name = "pipe_file.txt"
-os.mkfifo(pipe_name)
-#Escribir al pipe con nombre
-pipe_file = open(pipe_name, "w")
-pipe_file.write("Hola, mundo!")
-pipe_file.close()
+pipe1_name = "/tmp/pipe1"
+pipe2_name = "/tmp/pipe2"
+pipe3_name = "/tmp/pipe3"
+pipe4_name = "/tmp/pipe4"
 
-# Leer del pipe con nombre
-pipe_file = open(pipe_name, "r")
-data = pipe_file.read()
-print(data)
-pipe_file.close()
+p = os.fork()
 
+if(p == 0):
+    #3 y 4
+    pipe_file_read = open(pipe3_name, "r")
+    pipe_file_write = open(pipe4_name, "w")
+else:
+    #1 y 2
+    pipe_file_read = open(pipe1_name, "r")
+    pipe_file_write = open(pipe2_name, "w")
 
-# Crear el archivo de lectura y escritura
-#pipe_file = open("pipe_file.txt", "w+")
+while(1):
+    recieve = pipe_file_read.read()
+    if(recieve == 49):
+        if(p == 0):
+            print("Se recibió correctamente desde el proceso hijo")
+        else:
+            print("Se recibió correctamente desde el proceso padre")
+    else:
+        if(p == 0):
+            print("No se recibió correctamente el dato, soy el proceso hijo")
+        else:
+            print("No se recibió correctamente el dato, soy el proceso padre")
+    #hacer proceso de la red
+    pipe_file_write.write("1")
 
-# Escribir un mensaje en el archivo
-#message = "Hola, mundo!"
-#pipe_file.write(message)
-#pipe_file.flush()
-
-# Mover el puntero al inicio del archivo
-#pipe_file.seek(0)
-
-# Leer del archivo
-#data = pipe_file.read()
-#print(data)
-
-# Cerrar el archivo
-#pipe_file.close()
-
-
-
+pipe_file_read.close()
+pipe_file_write.close()
--- load_image.py
+++ load_image.py
@@ -1,57 +1,86 @@
 import torch
 from PIL import Image
 from datetime import datetime
+import os
 
 # Cargar modelo entrenado
 modelo = torch.hub.load('/home/root/img192v3/yolov5/','custom', path='/home/root/img192v3/yolov5/runs/train/exp/weights/best-fp16.tflite',source='local')
 print("Modelo cargado")
 
-# Cargar imagen
-img_path = "/home/root/img192v3/yolov5/train/images/image13_png.rf.7298cf76bfa678154edb426a2134065b.jpg"
-#img_path = "/home/root/image5.png"
-img = Image.open(img_path)
+# Crear pipe con nombre
+pipe1_name = "/tmp/pipe1"
+pipe2_name = "/tmp/pipe2"
+pipe3_name = "/tmp/pipe3"
+pipe4_name = "/tmp/pipe4"
 
-#img_crop = img.crop((54,207,261,0))
+p = os.fork()
+
+if(p == 0):
+    #3 y 4
+    pipe_file_read = open(pipe3_name, "r")
+    pipe_file_write = open(pipe4_name, "w")
+else:
+    #1 y 2
+    pipe_file_read = open(pipe1_name, "r")
+    pipe_file_write = open(pipe2_name, "w")
+
+while(1):
+    recieve = pipe_file_read.read()
+    #hacer proceso de la red
+
+    # Cargar imagen
+    img_path = "/home/root/img192v3/yolov5/train/images/image13_png.rf.7298cf76bfa678154edb426a2134065b.jpg"
+    #img_path = "/home/root/image5.png"
+    img = Image.open(img_path)
+
+    #img_crop = img.crop((54,207,261,0))
+        
+    print(f"\n Empezando result0 {datetime.now()}\n")
+    result0 = modelo(img, size=192)
+    matriz0 = result0.pandas().xyxy[0]
+    print(matriz0)
+    print(type(matriz0))
+    print(f"\nFin result0 {datetime.now()}\n")
+
+    img_path = "/home/root/img192v3/yolov5/train/images/image88_png.rf.b6d316e921e059c31afa59c6c730edc1.jpg"
+    img = Image.open(img_path)
+
+    print(f"\nEmpezando result1 {datetime.now()}\n")
+    result1 = modelo(img, size=192)
+    matriz1 = result1.pandas().xyxy[0]
+    print(matriz1)
+    print(type(matriz1))
+    print(f"\nFin result1 {datetime.now()}\n")
+
+    xmin_result0 = result0.pandas().xyxy[0]['xmin']
+    xmax_result0 = result0.pandas().xyxy[0]['xmax']
+    name_result0 = result0.pandas().xyxy[0]['name']
+
+    xmin_result1 = result1.pandas().xyxy[0]['xmin']
+    xmax_result1 = result1.pandas().xyxy[0]['xmax']
+    name_result1 = result1.pandas().xyxy[0]['name']
+
+    xmin_array0 = list(xmin_result0)
+    xmax_array0 = list(xmax_result0)
+    name_array0 = list(name_result0)
+
+    xmin_array1 = list(xmin_result1)
+    xmax_array1 = list(xmax_result1)
+    name_array1 = list(name_result1)
     
-print(f"\n Empezando result0 {datetime.now()}\n")
-result0 = modelo(img, size=192)
-matriz0 = result0.pandas().xyxy[0]
-print(matriz0)
-print(type(matriz0))
-print(f"\nFin result0 {datetime.now()}\n")
+    for xmin, xmax, name in zip(xmin_array0, xmax_array0, name_array0):
+        xprom = (xmin + xmax) / 2
+        print(f"R0:{name}:{xprom}")
+    for xmin, xmax, name in zip(xmin_array1, xmax_array1, name_array1):
+        xprom = (xmin + xmax) / 2
+        print(f"R1:{name}:{xprom}")
+        
+    #fin proceso red
+    pipe_file_write.write("1")
 
-img_path = "/home/root/img192v3/yolov5/train/images/image88_png.rf.b6d316e921e059c31afa59c6c730edc1.jpg"
-img = Image.open(img_path)
+pipe_file_read.close()
+pipe_file_write.close()
 
-print(f"\nEmpezando result1 {datetime.now()}\n")
-result1 = modelo(img, size=192)
-matriz1 = result1.pandas().xyxy[0]
-print(matriz1)
-print(type(matriz1))
-print(f"\nFin result1 {datetime.now()}\n")
-
-xmin_result0 = result0.pandas().xyxy[0]['xmin']
-xmax_result0 = result0.pandas().xyxy[0]['xmax']
-name_result0 = result0.pandas().xyxy[0]['name']
-
-xmin_result1 = result1.pandas().xyxy[0]['xmin']
-xmax_result1 = result1.pandas().xyxy[0]['xmax']
-name_result1 = result1.pandas().xyxy[0]['name']
-
-xmin_array0 = list(xmin_result0)
-xmax_array0 = list(xmax_result0)
-name_array0 = list(name_result0)
-
-xmin_array1 = list(xmin_result1)
-xmax_array1 = list(xmax_result1)
-name_array1 = list(name_result1)
- 
-for xmin, xmax, name in zip(xmin_array0, xmax_array0, name_array0):
-    xprom = (xmin + xmax) / 2
-    print(f"R0:{name}:{xprom}")
-for xmin, xmax, name in zip(xmin_array1, xmax_array1, name_array1):
-    xprom = (xmin + xmax) / 2
-    print(f"R1:{name}:{xprom}")
 
 # Cerrar el archivo                                                                            
                                                               
