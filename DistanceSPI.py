75da1fe2461116caf16dfd6f8b2ef36a86924d23
IE729892 <ie729892@iteso.mx>
2024-05-11 11:40:09 -0600

    AAAAAAAAAAAAAAAAAAA

--- DistanceSPI.py
+++ DistanceSPI.py
@@ -23,6 +23,7 @@ DATA_1 = []
 DATA_2 = []
 DATA_F = []
 
+spi = spidev.SpiDev()
 
 def processObj(line):
     parts = line.split(":")
@@ -48,17 +49,14 @@ while(1):
                 print("Para out.ppm se esperan " + str(ou1_obj) + " objetos")
                 if(ou1_obj != 0):
                     counter1 = counter1 + 1
-                else:
-                    ou1_flag = 1
                 break
             print("RECIBIMOS DESDE out.ppm o PADRE:")
             [class1, xmin1, xprom1, xmax1, ymin1] = processObj(recieve)
-            DATA_1.append([class1, xmin1, xprom1, xmax1, ymin1])
+            DATA_1[line] = [class1, xmin1, xprom1, xmax1, ymin1]
             counter1 = counter1 + 1
             if(counter1 == ou1_obj+1):
                 ou1_flag = 1
                 counter1 = 0
-                DATA_1 = []
             break
 
     if(ou2_flag == 0):
@@ -69,18 +67,15 @@ while(1):
                 print("Para out2.ppm se esperan " + str(ou2_obj) + " objetos")
                 if(ou2_obj != 0):
                     counter2 = counter2 + 1
-                else:
-                    ou2_flag = 1
                 break
             print("RECIBIMOS DESDE out2.ppm o HIJO:")
             #Falta poner los objetos que recibe en lo que se te antoje y despues ya lo de la distancia
             [class2, xmin2, xprom2,xmax2, ymin2] = processObj(recieve2)
-            DATA_2.append([class2, xmin2, xprom2, xmax2, ymin2])
+            DATA_2[line2] = [class2, xmin2, xprom2, xmax2, ymin2]
             counter2 = counter2 + 1
             if(counter2 == ou2_obj+1):
                 ou2_flag = 1
                 counter2 = 0
-                DATA_2 = []
             break
 
     if(ou1_flag == 1 and ou2_flag == 1):
@@ -91,9 +86,7 @@ while(1):
         #CLASS_NUM
         #For xmin,xprom and xmax LEFT(0),CENTER(1),RIGHT(2)
         #For Distance CLOSE(0),MID(1),FAR(2)
-        if(ou1_obj == ou2_obj):
-            print("COINCIDE! EMPEZANDO PROCESAMIENTO Y TRANSFERENCIA")
-            spi = spidev.SpiDev()
+        if(len(DATA_1) == len(DATA_2)):
             spi.open(0, 0)
             spi.xfer([len(DATA_1)])
             spi.close()
@@ -104,7 +97,6 @@ while(1):
                 xprom2 = DATA_2[i][2]
                 result_f = (xprom1 / xprom2) * 7
                 if (xclass1 == xclass2):
-                    spi = spidev.SpiDev()
                     spi.open(0, 0)
                     DATA_F.append(xclass1)
                     DATA_F.append(result_f)
@@ -115,7 +107,6 @@ while(1):
                         spi.close()
         else:
             print("No se pueden comparar los objetos")
-            spi = spidev.SpiDev()
             spi.open(0, 0)
             spi.xfer(200)
             spi.close()
